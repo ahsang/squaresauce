0cd514d13a4c5e72697ac624f5df69a51ad87bc6
Awais Abdul Khaliq <awais.a.khaliq@gmail.com>
2015-08-04 13:06:58 +0500

    Enable facebook login

--- config/passport.js
+++ config/passport.js
@@ -4,8 +4,17 @@ var _ = require('lodash');
 var _super = require('sails-auth/config/passport');
 
 _.merge(exports, _super);
-_.merge(exports, {
+_.merge(exports.passport, {
 
   // Extend with custom logic here by adding additional fields, methods, etc.
-
+ facebook: {
+    name: 'Facebook',
+    protocol: 'oauth2',
+    strategy: require('passport-facebook').Strategy,
+    options: {
+      clientID: '436918573176696',
+      clientSecret: 'acfc7bd6a5eb4eecbd684ce4bd249a13',
+      profileFields: ['id', 'first_name', 'last_name', 'email'] 
+    }
+  }
 });
