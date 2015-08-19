e484c299f27c1705a52f6c23d52ca1bb9a16a7e4
ahsangondal <ahsangondal15@gmail.com>
2015-08-19 22:15:40 +0500

    abcd

--- config/env/production.js
+++ config/env/production.js
@@ -16,8 +16,8 @@ module.exports = {
    * Set the default database connection for models in the production        *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-   proxyHost: 'squaresauce.herokuapp.com',
-   proxyPort: 443
+   // proxyHost: 'squaresauce.herokuapp.com',
+   // proxyPort: 443
   // models: {
   //   connection: 'someMysqlServer'
   // },
