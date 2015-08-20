1a9b166e987f7ae582756266cd99ab555dddcb3d
Tashfeen Ahmed <tashfene@gmail.com>
2015-08-20 17:51:48 +0500

    This is Square :p
    Landing.

--- assets/js/specific/main.js
+++ assets/js/specific/main.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
 			slidesNumber = slidesWrapper.children('li').length,
 			visibleSlidePosition = 0,
 			autoPlayId,
-			autoPlayDelay = 1000000;
+			autoPlayDelay = 100000000;
 
 		//upload videos (if not on mobile devices)
 		uploadVideo(slidesWrapper);
