4c4ca7151c2220ded79585042e7875f6d54c9287
ahsangondal <ahsangondal15@gmail.com>
2015-08-20 12:23:38 +0500

    mongo
    commit

--- config/connections.js
+++ config/connections.js
@@ -58,11 +58,11 @@ module.exports.connections = {
   ***************************************************************************/
   someMongodbServer: {
     adapter: 'sails-mongo',
-    host: 'localhost',
+    host: 'ds035573.mongolab.com',
     port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    user: 'ahsan',
+    password: 'ahsan',
+    database: 'heroku_k6vk7zxk'
   },
 
   /***************************************************************************
