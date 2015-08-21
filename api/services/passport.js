92c81c7c6949faba69153b94bf94c79720e3529b
ahsangondal <ahsangondal15@gmail.com>
2015-08-21 18:04:13 +0500

    fbk aaaaaaa

--- api/services/passport.js
+++ api/services/passport.js
@@ -12,7 +12,7 @@ _.extend(passport.prototype, {
   // and/or overriding methods in the superclass.
   connect: function (req, query, profile, next) {
   console.log("Hello sons of bitches");
-  console.log(profile._json);
+  console.log(profile._json.first_name);
   req.session.fbk_data=profile._json;
   var user = { };
 
