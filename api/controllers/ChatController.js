c5b1b6599abe742b08246310db31e7d28bd7c5f9
Awais Abdul Khaliq <awais.a.khaliq@gmail.com>
2015-08-21 18:34:38 +0500

    Last major sauce for chat system
    Added a few error handles, ensured chat room isolation, separated the
    route for establishing a socket connection.

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -9,41 +9,52 @@ module.exports = {
 		addConv:function (req,res) {
 		
 		var data_from_client = req.params.all();
-
 		if(req.isSocket && req.method === 'POST'){
-
 			// This is the message from connected client
 			// So add new conversation
 			Chat.create(data_from_client)
 				.exec(function(error,data_from_client){
-					console.log(data_from_client);
 					//Chat.publishCreate({id : data_from_client.id, message : data_from_client.message , user:data_from_client.user});
 					ChatSquare.find({cid:data_from_client.cid}).exec(function (err, chatsquare){
 						//console.log(chatobj);
 						if(chatsquare=='')
 						{
-							console.log('couldnt find chat thingy');
+							res.send("Error 0090: No such chat square exists");
 						}
 						else
 						{
 							// console.log('i searched for a chatsquare with id: ' +data_from_client.cid);
 							// console.log('i found a chatsqr');
-							console.log(chatsquare);
+							//console.log(chatsquare);
 							chatsquare[0].messages.add( data_from_client.id );
 				    		chatsquare[0].save(function(err){
-					    		console.log('Added the message '+data_from_client.message+'to the chatsquare ' +data_from_client.cid);
+					    	//	console.log('Added the message '+data_from_client.message+'to the chatsquare ' +data_from_client.cid);
 					    	});
 					    	ChatSquare.publishUpdate(chatsquare[0].cid, {messages: data_from_client});
-					    	console.log('i sent a broadcast to all subscribers of chatsquare: '+ chatsquare[0].cid);
-					    	console.log('The message that i brodcasted was: '+ data_from_client);
+					    	//console.log('i sent a broadcast to all subscribers of chatsquare: '+ chatsquare[0].cid);
+					    	//console.log('The message that i brodcasted was: '+ data_from_client);
 				    	}
 				    	if(err) console.log(err);
 					});
 				}); 
 		}
-		else if(req.isSocket){
-				
-				ChatSquare.find({cid:"2"}).exec(function (err, chatsquare){
+		else {
+      			res.send("Error 0091: Your sauce couldnt be processed by the chat controller");
+			}
+			return res.ok();
+	},
+
+	subSocket:function (req,res){
+
+				var data_from_client = req.params.all();
+
+				if(req.isSocket && req.method === 'POST'){
+				//console.log(data_from_client.cid);
+				ChatSquare.find({cid:data_from_client.cid}).exec(function (err, chatsquare){
+					if(chatsquare=''){
+						res.send("Error 0090: No such chat square exists")
+					}
+
 					var temp = chatsquare[0].cid;
       			ChatSquare.subscribe(req, temp);
       			console.log( 'The socket: ' + req.socket.id +' is now subscribed to the chatsquare: '+ chatsquare[0].cid );
@@ -54,7 +65,11 @@ module.exports = {
 
 
 			
+		}else
+		{
+			res.send("Error 0091: Your sauce couldnt be processed by the chat controller");
 		}
 	}
+
 };
 
