0b1a0b7d5320e9f724b6679f9d5684a2cb4ae757
ahsangondal <ahsangondal15@gmail.com>
2015-08-24 02:09:16 +0500

    getsquareusers

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -16,7 +16,32 @@ module.exports = {
   
 	},
 	viewSquare: function(req,res){
+
+		//name,fbk_id,is_admin,username
 		res.view('square');
+	},
+	getSquareUsers: function(req,res){
+		if(req.param('sname')){
+			var a=new Array();
+			ChatSquare.findOne({cid:req.param('sname')}).populateAll().then(function(found){
+				// if(err)console.log(err);
+
+				var users=found.users;
+				
+				
+				return [users];
+				// console.log(a);
+			}).spread(function(abc){
+				console.log(abc);
+				res.send(abc);
+			}).catch(function(err){
+				console.log(err);
+			});
+		}else{
+			res.send("Please send me a square name brother");
+		}
 	}
+
+
 };
 
